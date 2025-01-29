277ed722628db1b2c2e4655dfc34df75038aa14d
Ricardo Guerra <RikkuG491@gmail.com>
2025-01-29 14:33:42 -0500

    Added solution code for Deliverables

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 function Pet({pet}){
+
+    // Deliverable # 1 solution code
+    const [count, setCount] = useState(0);
+
+    // Deliverable # 4 solution code
+    const [displayAnimalType, setDisplayAnimalType] = useState(false);
+
     return (
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
-            <h4 className="">{pet.name}</h4>
-            <button className="like-button">0 Likes</button>
+
+            {/* Deliverable # 5 & 6 solution code */}
+            <h4 onClick={() => setDisplayAnimalType(!displayAnimalType)} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
+
+            {/* Deliverable # 2 & 3 solution code */}
+            <button onClick={() => setCount(count + 1)} className="like-button">{count} Likes</button>
         </li>
     );
 }
