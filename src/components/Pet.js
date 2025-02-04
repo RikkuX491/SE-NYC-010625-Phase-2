5555b25f87e26206e86beb672361fb88cbd9ac75
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-04 16:23:57 -0500

    Updated starter code in Pet.js

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -26,18 +26,19 @@ function Pet({pet, deletePet, updatePet}){
             <img src={pet.image} alt={pet.name}/>
             <h4 onClick={toggleDisplayAnimalType} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
             { true ?
-            <div className="button-div">
-                <button className="update-button">Update Pet</button>
-                <button onClick={handleLikeButtonClick} className="like-button">{pet.likes} Likes</button>
-                <br/>
-                <button onClick={handleAdoptButtonClick} className="adopt-button">Adopt</button>
-            </div> :
             <form className="edit-pet">
                 <input type="text" name="name" placeholder="Pet name" />
                 <input type="text" name="image" placeholder="Image URL" />
                 <input type="text" name="animal_type" placeholder="Animal type" />
                 <button type="submit">Save Changes</button>
             </form>
+            :
+            <div className="button-div">
+                <button className="update-button">Update Pet</button>
+                <button onClick={handleLikeButtonClick} className="like-button">{pet.likes} Likes</button>
+                <br/>
+                <button onClick={handleAdoptButtonClick} className="adopt-button">Adopt</button>
+            </div>
             }
         </li>
     );
